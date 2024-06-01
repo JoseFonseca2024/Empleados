2c52eb37fa98ea21026005cdc2763a52f1f9edcd
Jose Gabriel Fonseca Cortez <167927128+JoseFonseca2024@users.noreply.github.com>
2024-05-31 22:10:57 -0600

    Corrección

--- src/Empleado.java
+++ src/Empleado.java
@@ -12,7 +12,7 @@ abstract class Empleado {
         return nombre;
     }
 
-    public String numIdentificación () {
+    public String getnumIdentificación () {
         return numIdentificación;
     }
 
